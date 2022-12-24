620465bffde43fa79bf1b20932390b1f9f0d75e2
Cody W. Eilar <cody@codyeilar.com>
2022-12-23 17:10:30 -1000

    Move to lua
    - Part of this commit is move all of my configs to a Lua based system.
      However, I am simultaneously trying to test out the lsp server that
      comes with vim too, instead of using the CoC thing I was using in the
      main branch. I am going to test this branch for a little bit and see
      if I like it. It has the downside of possibly slowing me down because
      I will be switching between versions of my configurations. But in
      the end, it may be worth it.
    
    Signed-off-by: Cody W. Eilar <cody@codyeilar.com>

--- .config/nvim/after/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
-local cmd = vim.cmd
-
-o.spell = true
--- cmd(':LanguageToolCheck')
---
+++ .config/nvim/lua/acidleroy/init.lua
@@ -0,0 +1,6 @@
+print("This is AcidLeroy directory")
+require("acidleroy.set")
+require("acidleroy.remap")
+require("acidleroy.packer")
+
+
