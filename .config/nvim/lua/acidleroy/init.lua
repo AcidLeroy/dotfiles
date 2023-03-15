62008458015e6dd7600aec05da78155950c3dcf4
Cody W. Eilar <cody@codyeilar.com>
2023-03-15 09:49:30 -0700

    Format *.go files on save
    Signed-off-by: Cody W. Eilar <cody@codyeilar.com>

--- .config/nvim/lua/acidleroy/init.lua
+++ .config/nvim/lua/acidleroy/init.lua
@@ -3,3 +3,17 @@ require("acidleroy.set")
 require("acidleroy.remap")
 require("acidleroy.packer")
 
+local augroup = vim.api.nvim_create_augroup
+local AcidLeroyGroup = augroup('AcidLeroy',{})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+local fmt = function() vim.lsp.buf.format({ async = false }) end
+
+-- Automatically format go files on save
+ autocmd({"BufWritePre"}, {
+   group = AcidLeroyGroup,
+   pattern = {"*.go"},
+   callback = fmt
+ })
+
